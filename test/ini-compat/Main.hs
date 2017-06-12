6def6c6b95115eaae809f2d94111253619cf37ed
Getty Ritter <gettyritter@gmail.com>
2017-06-12 13:28:12 -0700

    Updates for new Hedgehog versions

--- test/ini-compat/Main.hs
+++ test/ini-compat/Main.hs
@@ -1,13 +1,17 @@
+{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Main where
 
 import qualified Data.Foldable as Fold
+import           Data.Function (on)
 import           Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 import qualified Data.Ini as I1
 import qualified Data.Ini.Config.Raw as I2
-import           Data.List (nub)
+import           Data.List (nubBy)
 import qualified Data.Sequence as Seq
 import           Data.Text (Text)
 import qualified Data.Text as T
@@ -16,8 +20,8 @@ import           Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
-prop_iniEquiv :: Property
-prop_iniEquiv = property $ do
+propIniEquiv :: Property
+propIniEquiv = property $ do
   raw <- forAll mkIni
   let printed = I1.printIniWith I1.defaultWriteIniSettings raw
       i1 = I1.parseIni printed
@@ -29,8 +33,8 @@ prop_iniEquiv = property $ do
      in i1'' === i2''
    _ -> failure
 
-prop_revIniEquiv :: Property
-prop_revIniEquiv = property $ do
+propRevIniEquiv :: Property
+propRevIniEquiv = property $ do
   raw <- forAll mkRichIni
   let printed = I2.printIni raw
       i1 = I1.parseIni printed
@@ -40,8 +44,8 @@ prop_revIniEquiv = property $ do
      lower i1' === toMaps i2'
    _ -> failure
 
-prop_iniSelfEquiv :: Property
-prop_iniSelfEquiv = property $ do
+propIniSelfEquiv :: Property
+propIniSelfEquiv = property $ do
   raw <- forAll mkRichIni
   Right (toMaps raw) === fmap toMaps (I2.parseIni (I2.printIni raw))
 
@@ -69,7 +73,7 @@ mkIni = do
   return (I1.Ini (HM.fromList ss))
 
 mkComments :: Monad m => Gen m (Seq.Seq I2.BlankLine)
-mkComments = fmap (Seq.fromList . nub) $ Gen.list (Range.linear 0 5) $
+mkComments = fmap Seq.fromList $ Gen.list (Range.linear 0 5) $
   Gen.choice
     [ return I2.BlankLine
     , I2.CommentLine <$> Gen.element ";#" <*> textChunk
@@ -77,9 +81,9 @@ mkComments = fmap (Seq.fromList . nub) $ Gen.list (Range.linear 0 5) $
 
 mkRichIni :: Monad m => Gen m I2.Ini
 mkRichIni = do
-  ss <- Gen.list (Range.linear 0 10) $ do
+  ss <- Gen.list (Range.linear 0 100) $ do
     name <- textChunk
-    section <- Gen.list (Range.linear 0 10) $ do
+    section <- Gen.list (Range.linear 0 100) $ do
       k <- textChunk
       v <- textChunk
       cs <- mkComments
@@ -88,11 +92,15 @@ mkRichIni = do
              )
     cs <- mkComments
     return ( T.toLower name
-           , I2.IniSection name (Seq.fromList section) 0 0 cs
+           , I2.IniSection name (Seq.fromList (nubBy ((==) `on` fst) section)) 0 0 cs
            )
-  return (I2.Ini (Seq.fromList ss))
+  return (I2.Ini (Seq.fromList (nubBy ((==) `on` fst) ss)))
 
 main :: IO ()
 main = do
-  _ <- $$(checkConcurrent)
+  _ <- checkParallel $ Group "Test.Example"
+         [ ("propIniEquiv", propIniEquiv)
+         , ("propRevIniEquiv", propRevIniEquiv)
+         , ("propIniSelfEquiv", propIniSelfEquiv)
+         ]
   return ()
