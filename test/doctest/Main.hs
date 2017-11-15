5bd05fa2f018a8eb35122b8b97bbfadaa568c1cd
Getty Ritter <gettyritter@gmail.com>
2017-11-15 00:56:38 -0800

    Remove St from doctest list

--- test/doctest/Main.hs
+++ test/doctest/Main.hs
@@ -5,4 +5,3 @@ import Test.DocTest (doctest)
 main :: IO ()
 main = do
   doctest [ "src/Data/Ini/Config.hs", "-XOverloadedStrings"  ]
-  doctest [ "src/Data/Ini/Config/St.hs", "-XRankNTypes", "-XOverloadedStrings" ]
