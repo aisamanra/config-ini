f68899cd1da923f58c5b3fea31afaa6f45662866
Kevin Quick <kquick@galois.com>
2018-09-10 16:38:59 -0700

    Fix doctest to find imports.

--- test/doctest/Main.hs
+++ test/doctest/Main.hs
@@ -4,4 +4,4 @@ import Test.DocTest (doctest)
 
 main :: IO ()
 main = do
-  doctest [ "src/Data/Ini/Config.hs", "-XOverloadedStrings"  ]
+  doctest [ "src/Data/Ini/Config.hs", "-XOverloadedStrings", "-isrc" ]
