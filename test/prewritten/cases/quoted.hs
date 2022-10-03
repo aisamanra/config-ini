6b08ad02e68a5fbb1b5462495760ddaee8e71fa5
Getty Ritter <tristero@infinitenegativeutility.com>
2022-10-03 13:55:30 -0700

    add strings with interior quotes test

---
+++ test/prewritten/cases/quoted.hs
@@ -0,0 +1,9 @@
+fromList
+  [ ( "s1",
+      fromList
+        [ ("single", "with 'single' quotes"),
+          ("double", "with \"double\" quotes"),
+          ("complex", "security find-generic-password -s \"matterhorn\" -a \"myaccount\" -w")
+        ]
+    )
+  ]
